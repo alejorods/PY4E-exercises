0721a5b06b073bb2fc55448c8bb52714d53d5453
Alejandro <jesusrods312@gmail.com>
2021-01-07 22:12:14 -0300

    Chapter 2 exercises

---
+++ exercise-2.2.py
@@ -0,0 +1,4 @@
+# EXERCISE 2.2: A program that uses input to prompt a user for their name and the welcomes them.
+
+name = input('Enter your name: ')
+print('Hello', name)
---
+++ exercise-2.3.py
@@ -0,0 +1,8 @@
+# EXERCISE 2.3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+hours = float(input('Enter hours: '))
+rate = float(input('Enter rate: '))
+
+pay = hours * rate
+
+print(pay)
