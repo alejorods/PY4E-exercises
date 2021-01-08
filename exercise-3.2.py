f8671e6c6b9ea2e61eee89056aae52c0cd2d7bc5
Alejandro <jesusrods312@gmail.com>
2021-01-07 22:27:55 -0300

    Chapter 3 exercises

---
+++ exercise-3.2.py
@@ -0,0 +1,13 @@
+# EXERCISE 3.2: Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program.
+
+try:
+    hours = float(input('Enter hours: '))
+    rate = float(input('Enter rate: '))
+    if hours > 40:
+        pay = (1.5 * (hours - 40) + 40) * rate
+        print(pay)
+    else:
+        pay = hours * rate
+        print(pay)
+except:
+    print('Error, please enter numeric input')
---
+++ exercise-3.3.py
@@ -0,0 +1,18 @@
+# EXERCISE 3.3: Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table: >= 0.9 A; >= 0.8 B; >= 0.7 C; >= 0.6 D; < 0.6 F.
+
+try:
+    score = float(input('Enter a score between 0.0 and 1.0: '))
+    if score < 0.6:
+        print('F')
+    elif score < 0.7:
+        print('D')
+    elif score < 0.8:
+        print('C')
+    elif score < 0.9:
+        print('B')
+    elif score <= 1.0:
+        print('A')
+    else:
+        print('Bad score')
+except:
+    print('Bad score')
