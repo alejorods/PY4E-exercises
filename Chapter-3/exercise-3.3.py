535bec29cad53a2fd6300ab276e40fc25934cf6c
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-14 18:31:55 -0300

    Update exercise-3.3.py

--- Chapter-3/exercise-3.3.py
+++ Chapter-3/exercise-3.3.py
@@ -3,15 +3,15 @@ If the score is between 0.0 and 1.0, print a grade using the following table: >=
 
 try:
     score = float(input('Enter a score between 0.0 and 1.0: '))
-    if score < 0.6:
+    if score >= 0 and score < 0.6:
         print('F')
-    elif score < 0.7:
+    elif score >= 0.6 and score < 0.7:
         print('D')
-    elif score < 0.8:
+    elif score >= 0.7 and score < 0.8:
         print('C')
-    elif score < 0.9:
+    elif score >= 0.8 and score < 0.9:
         print('B')
-    elif score <= 1.0:
+    elif score >= 0.9 and score <= 1.0:
         print('A')
     else:
         print('Bad score')
