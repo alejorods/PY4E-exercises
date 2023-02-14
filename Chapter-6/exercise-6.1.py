7af2ab1df8d79ea4eea665f22ae3acde68e07764
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-14 11:48:04 -0300

    Update exercise-6.1.py

--- Chapter-6/exercise-6.1.py
+++ Chapter-6/exercise-6.1.py
@@ -1,4 +1,5 @@
-# EXERCISE 6.1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.
+'''EXERCISE 6.1: Write a while loop that starts at the last character in the string and works its way backwards 
+to the first character in the string, printing each letter on a separate line, except backwards.'''
 
 string = input('Enter a word: ')
 
