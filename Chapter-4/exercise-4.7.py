d1c9e28f0eb23b8b3e880872371b942da87e4d7b
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-14 11:50:40 -0300

    Update exercise-4.7.py

--- Chapter-4/exercise-4.7.py
+++ Chapter-4/exercise-4.7.py
@@ -1,4 +1,5 @@
-# EXERCISE 4.7: Rewrite the grade program from the previous chapter using a function called computegrade that takes a score as its parameter and returns a grade as a string.
+'''EXERCISE 4.7: Rewrite the grade program from the previous chapter using a function called 
+computegrade that takes a score as its parameter and returns a grade as a string.'''
 
 def computegrade(score):
     try:
