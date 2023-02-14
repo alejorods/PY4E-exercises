0860b74a5348270b88902c26e8d9e83c77d08893
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-14 11:52:20 -0300

    Update exercise-5.1.py

--- Chapter-5/exercise-5.1.py
+++ Chapter-5/exercise-5.1.py
@@ -1,4 +1,6 @@
-# EXERCISE 5.1: Write a program which repeatedly reads numbers until the user enters “done”. Once “done” is entered, print out the total, count, and average of the numbers. If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
+'''EXERCISE 5.1: Write a program which repeatedly reads numbers until the user enters “done”. Once “done” is entered, print out the total, count, 
+and average of the numbers. If the user enters anything other than a number, detect their mistake using try and except and print an error message 
+and skip to the next number.'''
 
 total = 0
 count = 0
@@ -16,25 +18,3 @@ while True:
 print(total)
 print(count)
 print(total/count)
-
-
-# Incorrect way
-'''
-while True:
-    try:
-        total = 0
-        count = 0
-        user_number = input('Enter a number:')
-        number = float(user_number)
-        if user_number == 'done':
-            break
-        else:
-            count = count + 1
-            total = total + number
-    except:
-        print('Invalid input')
-print(total)
-print(count)
-print(total/count)
-'''
-# Here I have two mistakes. The first one is that the accumulator and the counter should be out of the loop. The second mistake is the position of the number variable. The program reads the input and try to convert it to a float type. Then, when we write 'done' there is an Error and the except acts.
