0920dd6f86104561136e6387502300426d632f79
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-14 11:54:03 -0300

    Update exercise-6.4.py

--- Chapter-6/exercise-6.4.py
+++ Chapter-6/exercise-6.4.py
@@ -1,4 +1,6 @@
-# EXERCISE 6.4: There is a string method called count that is similar to the function in the previous exercise. Read the documentation of this method at: https://docs.python.org/library/stdtypes.html#string-methods Write an invocation that counts the number of times the letter a occurs in “banana”.
+'''EXERCISE 6.4: There is a string method called count that is similar to the function in the previous exercise. Read the documentation of 
+this method at: https://docs.python.org/library/stdtypes.html#string-methods Write an invocation that counts the number of times the letter 
+a occurs in “banana”.'''
 
 word = 'banana'
 
