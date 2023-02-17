684147b37e9c394050d98518b05638e1f6ee83b0
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-17 16:04:59 -0300

    Update exercise-6.3.py

--- Chapter-6/exercise-6.3.py
+++ Chapter-6/exercise-6.3.py
@@ -1,4 +1,13 @@
-# EXERCISE 6.3: 
+'''EXERCISE 6.3: Encapsulate this code 
+
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+
+in a function named count, and generalize it so that it accepts the string and the letter as arguments.'''
 
 def count(word,letter):
     counter = 0
