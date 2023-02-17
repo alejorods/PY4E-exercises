6aef4a930b820134e15d7a0b759b424b9479df16
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-17 16:31:16 -0300

    Update exercise-6.5.py

--- Chapter-6/exercise-6.5.py
+++ Chapter-6/exercise-6.5.py
@@ -2,12 +2,8 @@
 the portion of the string after the colon character and then use the float function to convert the extracted string into a floating point number.'''
 
 data = 'X-DSPAM-Confidence:0.8475'
-
 zero_position = data.find('0')
 five_position = data.find('5')
-
 string = data[zero_position:]
-
 number = float(string)
-
 print(number)
