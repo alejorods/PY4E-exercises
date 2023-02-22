c6f398f2e303c1d141e02706b3d58184ba2b047d
Alejandro Rodríguez <73655071+alejorods@users.noreply.github.com>
2023-02-22 10:31:50 -0300

    Update exercise-9.1.py

--- Chapter-9/exercise-9.1.py
+++ Chapter-9/exercise-9.1.py
@@ -1,5 +1,9 @@
-'''EXERCISE 9.1: Download a copy of the file www.py4e.com/code3/words.txt Write a program that reads the words in words.txt and stores them as keys 
-in a dictionary. It doesn’t matter what the values are. Then you can use the in operator as a fast way to check whether a string is in the dictionary.'''
+'''EXERCISE 9.1: Download a copy of the file 
+
+www.py4e.com/code3/words.txt 
+
+Write a program that reads the words in words.txt and stores them as keys in a dictionary. It doesn’t matter what the values are. 
+Then you can use the in operator as a fast way to check whether a string is in the dictionary.'''
 
 fhand = open('/home/alejandro/Documentos/Desarrollo/python/PY4E-ejercicios/words.txt', 'r')
 dictionary = {}
